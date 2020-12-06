7c63e1073783a695b512ceced0b8bb35a126fbeb
Rildo Alves <junior.alvesp99@gmail.com>
2020-12-06 07:02:10 -0300

    Mapeamento da entidade gender

--- src/main/java/com/mobius/gametracking/entities/Gender.java
+++ src/main/java/com/mobius/gametracking/entities/Gender.java
@@ -1,9 +1,33 @@
 package com.mobius.gametracking.entities;
 
-public class Gender {
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "tb_gender")
+public class Gender implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
 	private String name;
+	
+	@JsonIgnore
+	@ManyToMany(mappedBy = "genders")
+	private Set<Game> games = new HashSet<>();
 	
 	public Gender() {
 		
@@ -28,6 +52,10 @@ public class Gender {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	public Set<Game> getGames() {
+		return games;
 	}
 
 	@Override
